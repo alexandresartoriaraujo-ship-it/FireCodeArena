125423ba61b379fef2f70caafe926e25198b2f13
alexandresartoriaraujo-ship-it <alexandre.sartoriaraujo@gmail.com>
2026-06-09 14:13:58 -0300

    subindo

---
+++ ex03.js
@@ -0,0 +1,18 @@
+const r = require("readline-sync")
+
+const idade = [14, 88, 110, 24, 67]
+
+for(let i = 0; i < 5; i++){
+    
+    if(idade[i] < 18){
+        console.log("Menor de idade: ")
+        console.log(idade[i])
+    } else if(idade[i] > 18){
+        console.log("Maior de idade: ")
+        console.log(idade[i])
+    } else if (idade[i] = 18){
+        console.log("Maior de idade: ")
+        console.log(idade[i])
+    }
+
+} 

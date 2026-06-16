df5272157491c4b2eddf7635a6bf8fff796960b1
alexandresartoriaraujo-ship-it <alexandre.sartoriaraujo@gmail.com>
2026-06-16 13:34:25 -0300

    subindo

---
+++ ex04.js
@@ -0,0 +1,9 @@
+const r = require ("readline-sync")
+
+let n = [3, 4, 5, 65, 53]
+
+for (let i = 0; i < n.length; i++){
+   console.log(n[i])
+}
+console.log('')
+console.log(`o maior numero entre eles é ${n[3]}`)

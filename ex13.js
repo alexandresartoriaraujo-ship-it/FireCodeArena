11555903ea1df4a2743bb7742e76e2ad50f71c47
alexandresartoriaraujo-ship-it <alexandre.sartoriaraujo@gmail.com>
2026-06-16 17:08:18 -0300

    subindo

---
+++ ex13.js
@@ -0,0 +1,19 @@
+const readline = require('readline-sync')
+
+let cadastroDeNumeros = readline.questionInt('Quantos numeros deseja cadastras? ');
+
+let multiplosDe3 = [];
+let outrosNumeros = [];
+
+for (let i = 0; i < cadastroDeNumeros; i++){
+    let numeros = readline.questionInt('Digite um numero: ');
+
+    if (numeros % 3 === 0) {
+    multiplosDe3.push(numeros);
+    } else {
+        outrosNumeros.push(numeros)
+    }
+}
+
+console.log(`Multiplos de 3: + ${multiplosDe3}`)
+console.log(`Outros numeros: + ${outrosNumeros}`)

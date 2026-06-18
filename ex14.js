31b80165ed37ec4b0ac982031f79213dc67bf3f4
alexandresartoriaraujo-ship-it <alexandre.sartoriaraujo@gmail.com>
2026-06-18 08:42:29 -0300

    subindo

--- ex14.js
+++ ex14.js
@@ -1,44 +1,48 @@
+
+// Peça ao usuário quantas notas deseja cadastrar.
+// Armazene todas em um vetor.
+// Ao final, mostre:
+// Maior nota
+// Menor nota
+// Média da turma
+// Quantidade de aprovados (nota ≥ 7)
+// Quantidade de reprovados (nota < 7)
+
 const r = require("readline-sync")
 
-let qtq = r.questionInt("Quantas notas deseja cadastras: ")
+let qtq = r.questionInt("Quantas notas deseja cadastrar: ")
 
-let notas  = []
+let notas = []
 let soma = 0
-let Aprovado = 0 
+let Aprovado = 0
 let Reprovado = 0
 
-for (let i = 0; i < qtq; i++){
-    console.log("")
-    let nota = r.questionInt(`Digite a ${i+1}° notas: `)
-    console.log("")
+for(let i = 0; i < qtq; i++){
+    let nota = r.questionInt(`Digite a ${i+1}° nota: `)
     notas.push(nota);
     soma = soma + nota;
 
-if(nota >= 7 ){
-    Aprovado++
-}else {
-    Reprovado++
-}
+    if(nota >= 7){
+        Aprovado++
+    
+    }else{
+        Reprovado++
+    }
 }
 
 let maior = notas [0]
 let menor = notas [0]
 
-for(let i = 1; i = notas.length; i++){
+for(let i = 1; i < notas.length; i++){
     if(notas[i] < menor)
         menor = notas[i]
 }
 
 let media = soma/qtq
 
-console.log("==== Resultado final ====")
-console.log("")
-console.log(`Maior nota: ${maior}`)
-console.log("")
-console.log(`Maior nota: ${menor}`)
-console.log("")
-console.log(`Media da turma: ${media}`)
-console.log("")
-console.log(`Quantidade de aprovados: ${Aprovado}`)
-console.log("")
-console.log(`Quantidade de Reprovados: ${Reprovado}`)
+console.log(`Maior nota: ${maior}`);
+console.log(`Menor nota: ${menor}`);
+console.log(`Media da turma: ${media}`);
+console.log(`Quantidade de aprovados: ${Aprovado}`);
+console.log(`Quantidade de reprovados: ${Reprovado}`)
+
